bcf8b1b3ff0e9cc04faca9312dd4b4bcca645e07
Sanskar Bansal <sanskarbnsl75@gmail.com>
2019-06-02 03:57:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,7 +21,7 @@ end
 
 def valid_move?(board, index)
   if(index >=0 && index <= 8)
-    if(board[index]!="X"||board[index]!="O")
+    if(board[index]!="X" || board[index]!="O")
       return true
     end
   end
