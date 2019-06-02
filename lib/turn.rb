b17d25aa6282e8465fd7d4e271f0c6451130934e
Sanskar Bansal <sanskarbnsl75@gmail.com>
2019-06-02 04:02:07 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,8 +32,6 @@ def input_to_index(number)
   return number.to_i-1
 end
 
-def move(board, pos, value="X")
-  if(valid_move?(board, pos)==true)
+def move(board, pos, value="X)
     board[pos]=value
-  end
 end
